d6915bdadaa2224a7fb86fdb8714c641afe51398
Viresh Patel <vpatel@williamhill.us>
2021-04-06 11:28:14 -0400

    added test file

---
+++ python-test-automation/test.py
@@ -0,0 +1,24 @@
+import logging
+import os
+
+# set file path and name
+dir_path = os.path.dirname(os.path.realpath(__file__))
+filename = os.path.join(dir_path, 'test_log.log')
+
+# logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler(filename)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
+
+
+def do_logging():
+    logger.info('test')
+
+
+if __name__ == '__main__':
+    do_logging()
